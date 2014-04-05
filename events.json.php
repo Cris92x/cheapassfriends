0207737b0718bc9fba2d412dbb58310b065bb4d8
stefanhchou <stefanhchou@gmail.com>
2014-04-05 14:49:10 -0500

    deleted unneeded files

--- events.json.php
+++ events.json.php
@@ -2,76 +2,36 @@
 	"success": 1,
 	"result": [
 		{
-			"id": "293",
-			"title": "wHAT",
+			"id": "100",
+			"title": "activity #1",
 			"url": "http://www.example.com/",
 			"class": "event-warning",
-			"start": "1362938400000",
-			"end":   "1363197686300"
+			"start": "1363155300000",
+			"end":   "1363155300000"
 		},
 		{
-			"id": "256",
-			"title": "AASAAS",
+			"id": "200",
+			"title": "activity #2",
 			"url": "http://www.example.com/",
 			"class": "event-warning",
 			"start": "1363155300000",
 			"end":   "1363227600000"
 		},
 		{
-			"id": "276",
-			"title": "ASSASA",
+			"id": "300",
+			"title": "activity #3",
 			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363245600000",
-			"end":   "1363252200000"
+			"class": "event-warning",
+			"start": "1363155300000",
+			"end":   "1363227600000"
 		},
 		{
-			"id": "294",
-			"title": "ASSASAS ",
+			"id": "400",
+			"title": "activity #4",
 			"url": "http://www.example.com/",
-			"class": "event-info",
-			"start": "1363111200000",
-			"end":   "1363284086400"
-		},
-		{
-			"id": "297",
-			"title": "SAASSSA",
-			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363234500000",
-			"end":   "1363284062400"
-		},
-		{
-			"id": "54",
-			"title": "ASSASAAS",
-			"url": "http://www.example.com/",
-			"class": "",
-			"start": "1363712400000",
-			"end":   "1363716086400"
-		},
-		{
-			"id": "532",
-			"title": "ASSAASAS",
-			"url": "http://www.example.com/",
-			"class": "event-inverse",
-			"start": "1364407200000",
-			"end":   "1364493686400"
-		},
-		{
-			"id": "548",
-			"title": "This is special event",
-			"url": "http://www.example.com/",
-			"class": "event-special",
-			"start": "1363197600000",
-			"end":   "1363629686400"
-		},
-		{
-			"id": "295",
-			"title": "Event 3",
-			"url": "http://www.example.com/",
-			"class": "event-important",
-			"start": "1364320800000",
-			"end":   "1364407286400"
+			"class": "event-warning",
+			"start": "1363155300000",
+			"end":   "1363227600000"
 		}
 	]
 }
